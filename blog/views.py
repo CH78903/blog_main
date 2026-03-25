e91bb203bc9573466e950a18013507cfd39f2ab9
CH78903 <cris0910lopez@gmail.com>
2026-03-24 17:14:35 -0700

    creacion de pagina

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,17 @@ from .models import Post, Category
 
 # Create your views here.
 def home(request):
-    Post = Post.objects.filter(status=Post.ACTIVE).orden_by('-created_at')
+    posts = Post.objects.filter(status=Post.ACTIVE).order_by('-created_at')
     context = {
-        'posts': Post
+        'posts': posts
     }
     return render(request, 'blog/home.html', context)
+
+def detail(resquet, id):
+    post = get_list_or_404(Post, id=id, status=Post.ACTIVE)
+
+
+    context = {
+       'post': post,
+    }
+    return render(resquet, 'blog/detail.html',context)
