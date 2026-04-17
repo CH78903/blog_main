19072fba224f1532216c4e1ceb1c2e06619285b5
CH78903 <cris0910lopez@gmail.com>
2026-04-16 17:19:41 -0700

    Modificacion

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
+#_*_ coding: -utf-8 _*_
+
 from django.db.models import Q
-from django.shortcuts import render, get_list_or_404, render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post, Category
+from .forms import CommentForm
 
-# Create your views here.
+#Create your views here.
 def home(request):
     posts = Post.objects.filter(status=Post.ACTIVE).order_by('-created_at')
     context = {
@@ -11,11 +14,23 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
-def detail(resquet, id):
-    post = get_list_or_404(Post, id=id, status=Post.ACTIVE)
+def detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.ACTIVE)
 
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail')
+        else:
+            form = CommentForm()
 
     context = {
-       'post': post,
+        'post': post,
+        'form': form
     }
-    return render(resquet, 'blog/detail.html',context)
+    return render(request, 'blog/detail.html', context)
